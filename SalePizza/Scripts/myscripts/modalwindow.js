ed09f9842ddf310c521e03eab8f61f72915cfd0e
Ilya <i.buevich@andersenlab.com>
2018-08-06 13:32:34 +0300

    All

---
+++ SalePizza/Scripts/myscripts/modalwindow.js
@@ -0,0 +1,15 @@
+﻿    //$(function () {
+    //    $.ajaxSetup({ cache: false });
+    //    $(".CartItem").click(function (e) {
+
+    //        e.preventDefault();
+    //        $.get(this.href, function (data) {
+    //            $('#dialogContent').html(data);
+    //            $('#modDialog').modal('show');
+    //        });
+    //    });
+    //})
+
+    $(document).on('click', '#purchase', function () {
+        $('#modal-content').modal('show');
+    });
