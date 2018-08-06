7f59db2984e8ea4f54ae5a43b4359f411ac83059
Ilya <i.buevich@andersenlab.com>
2018-08-06 14:43:56 +0300

    666

--- SalePizza/Models/PizzaContext.cs
+++ SalePizza/Models/PizzaContext.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.AspNet.Identity.EntityFramework;
 using SalePizza.FluentAPI;
 
 namespace SalePizza.Models
 {
-
     public class PizzaContext : IdentityDbContext<ApplicationUser>    //: DbContext 
     {
 
@@ -56,52 +53,6 @@ namespace SalePizza.Models
             modelBuilder.Configurations.Add(new FPizza());
             modelBuilder.Configurations.Add(new FPurchase());
         }
+
     }
-
-    public interface IRepository
-    {
-        void Save(Pizza b);
-        IEnumerable<Pizza> List();
-        Pizza Get(int id);
-    }
-
-    public class PizzaRepository : IDisposable, IRepository
-    {
-        private PizzaContext db = new PizzaContext();
-
-        public void Save(Pizza pizza)
-        {
-            db.Pizzas.Add(pizza);
-            db.SaveChanges();
-        }
-
-        public IEnumerable<Pizza> List()
-        {
-            return db.Pizzas;
-        }
-
-        public Pizza Get(int id)
-        {
-            return db.Pizzas.Find(id);
-        }
-
-        protected void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                if (db != null)
-                {
-                    db.Dispose();
-                    db = null;
-                }
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-    }
-
 }
